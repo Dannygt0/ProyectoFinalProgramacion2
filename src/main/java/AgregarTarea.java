0863f679f602e5ac384c0b63d5c9af91a220f219
Vielman Perez <vperezy1@miumg.edu.gt>
2025-11-13 18:44:20 -0600

    Opcion para agregar las tareas

---
+++ src/main/java/AgregarTarea.java
@@ -0,0 +1,35 @@
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AgregarTarea{
+
+    public static void agregarTarea() {
+        try {
+            Firestore db = ModificarTarea.conectarFirestore();
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("=== AGREGAR TAREA ===");
+            System.out.print("Ingrese descripción de la tarea: ");
+            String descripcion = in.nextLine();
+
+            //crea un mapa para guardar los datos
+            Map<String, Object> tarea = new HashMap<>();
+            tarea.put("descripcion", descripcion);
+            tarea.put("completada", false);
+
+            //Agrega el documento con ID automatico
+            ApiFuture<DocumentReference> future = db.collection("tareas").add(tarea);
+            DocumentReference docRef = future.get();
+
+            System.out.println("Tarea agregada exitosamente con ID: " + docRef.getId());
+
+        } catch (Exception e) {
+            System.out.println("Error al agregar la tarea: " + e.getMessage());
+        }
+    }
+}
